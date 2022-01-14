d39c25da8c68db09c89e0eb08d6257bd55565d34
Jackson-coder <jlxxlyh@163.com>
2022-01-14 13:10:36 +0800

    version_3

--- predict.py
+++ predict.py
@@ -9,12 +9,16 @@ from PIL import Image
 import cv2
 import numpy as np
 import torchvision
-from net.model import DenseNet121
+from net.model import DenseNet121, DenseNet121_torch_version
+
+CLASS_NAMES = ['Atelectasis', 'Cardiomegaly', 'Effusion', 'Infiltration', 'Mass', 'Nodule',
+               'Pneumonia', 'Pneumothorax', 'Consolidation', 'Edema', 'Emphysema', 'Fibrosis',
+               'Pleural_Thickening', 'Hernia']
 
 if __name__ == '__main__':
-    root = "/kaggle/input/data/images_003/images/00006199_010.png"
+    root = "data_pic/00000001_001.png"
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model_path = 'CheXNet.pth'
+
     img = Image.open(root).convert('RGB')
     image = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
@@ -25,11 +29,25 @@ if __name__ == '__main__':
                                                                                  std=[0.229, 0.224, 0.225])
                                                 ])
     img = transform(img)
-    model = DenseNet121().to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    model_path = 'weights/CheXNet.pth'
+    model = torch.load(model_path)
+    img = img.unsqueeze(0)
     model = model.eval()
-    result = model(img.to(device))
-    print(result)
+    results = model(img.to(device))
+
+    tensor_one = torch.Tensor([1] * 14).to(device)
+    tensor_zero = torch.Tensor([0] * 14).to(device)
+    results = torch.where(results > 0.5, tensor_one, tensor_zero)
+    results = results[0]
+
 
     im_color = cv2.applyColorMap(image, cv2.COLORMAP_JET)
+    t = 0
+    for n in range(len(results)):
+        if results[n] == 1:
+            print(CLASS_NAMES[n])
+            cv2.putText(im_color, str(CLASS_NAMES[n]), (100, 100 + t), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 8)
+            t += 100
+
     cv2.imwrite("img.png", im_color)
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch
 import csv
 from dataset import CheXDataset, data_generate
 from torch.utils import data
-from net.model import DenseNet121
+from net.model import DenseNet121, DenseNet121_torch_version
 from tqdm import tqdm
 from pytorchtools import EarlyStopping
 from torch.optim import lr_scheduler
@@ -29,27 +29,31 @@ if __name__ == '__main__':
     train_images, train_labels, val_images, val_labels = data_generate(reader)
     num_train = len(train_labels)
     num_val = len(val_labels)
-    train_batch_size = 128
+    train_batch_size = 32
 
     trainDataset = CheXDataset(train_images, train_labels)
     valDataset = CheXDataset(val_images, val_labels)
     trainData_iter = data.DataLoader(trainDataset, batch_size=train_batch_size, shuffle=True)
     valData_iter = data.DataLoader(valDataset, batch_size=1, shuffle=True)
 
-    model_loss = torch.nn.CrossEntropyLoss()
-    model = DenseNet121()
+    # model_loss = torch.nn.CrossEntropyLoss()
+    # model = DenseNet121()
+    model_loss = torch.nn.BCELoss(size_average=True)
+    model = DenseNet121_torch_version(14)
     model.to(device)
 
-
-    epoches = 1000
+    epoches = 40
     epoch_step_train = num_train // train_batch_size
     epoch_step_val = num_val // train_batch_size
 
-    patience = 10
-    T_max = 10
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    early_stopping = EarlyStopping(patience, verbose=True)
-    lr_scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min=0)
+    # patience = 10
+    # early_stopping = EarlyStopping(patience, verbose=True)
+    # T_max = 10
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    # lr_scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min=0)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5)
+    lr_scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=5, mode='min')
 
     for epoch in range(epoches):
         train_loss = 0
@@ -68,8 +72,8 @@ if __name__ == '__main__':
                 outputs = model(images.to(device))
 
                 loss = model_loss(outputs, labels.to(device))
-
-                loss = loss / len(outputs)
+                #
+                # loss = loss / len(outputs)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
@@ -85,6 +89,7 @@ if __name__ == '__main__':
         # 如果模型中有BN层(Batch Normalization）和Dropout，在测试时添加model.eval()。model.eval()是保证BN层能够用全部训练数据的均值
         # 和方差，即测试过程中要保证BN层的均值和方差不变。对于Dropout，model.eval() 是利用到了所有网络连接，即不进行随机舍弃神经元。
         with tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{epoches}', postfix=dict, mininterval=0.3) as pbar:
+            val_loss = 0
             for iteration, batch in enumerate(valData_iter):  # batch:(X, y)
                 if iteration > epoch_step_val:
                     break
@@ -93,10 +98,7 @@ if __name__ == '__main__':
 
                 outputs = model(images.to(device))
 
-                loss = 0
-                for i in range(len(outputs)):
-                    loss += model_loss(outputs[i], labels[i].to(device))
-                loss = loss / len(outputs)
+                loss = model_loss(outputs, labels.to(device))
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -106,21 +108,19 @@ if __name__ == '__main__':
                 pbar.set_postfix(**{'loss': val_loss / (iteration + 1),
                                     'lr': get_lr(optimizer)})
                 pbar.update(1)
+
         print('Finished Validation')
 
         print('Epoch:' + str(epoch + 1) + '/' + str(epoches))
-        print('Total Loss: %.3f || Val Loss: %.3f ' % (train_loss / epoch_step_train, val_loss / epoch_step_val))
+        print('Train Loss: %.3f || Val Loss: %.3f ' % (train_loss / epoch_step_train, val_loss / epoch_step_val))
 
-        if epoch % 10 == 0:
-            torch.save(model, 'weights/CheXNet_Val_Loss:_%.3f.pth' % (val_loss / epoch_step_val))
+        torch.save(model, 'weights/CheXNet_Val_Loss:_%.3f.pth' % (val_loss / epoch_step_val))
 
-        lr_scheduler.step()
-        early_stopping(val_loss / epoch_step_val, model)
-        if early_stopping.early_stop:
-            break
+        lr_scheduler.step(val_loss / epoch_step_val)
+        # early_stopping(val_loss / epoch_step_val, model)
+        # if early_stopping.early_stop:
+        #     break
 
     torch.save(model, 'weights/CheXNet.pth')
-
-
 
 # test123
